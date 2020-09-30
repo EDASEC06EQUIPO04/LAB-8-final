a4e97d338155925316a758291392835ef607995e
David Saenz <jdsccr2@hotmail.com>
2020-09-30 18:43:01 -0500

    requ 1 working, WIP aesthetic changes

--- App/view.py
+++ App/view.py
@@ -23,7 +23,9 @@
 import sys
 import config
 from DISClib.ADT import list as lt
+from DISClib.DataStructures import listiterator as it
 from App import controller
+import datetime
 assert config
 
 """
@@ -38,7 +40,39 @@ operación seleccionada.
 # ___________________________________________________
 
 
-crimefile = 'crime-utf8.csv'
+#crimefile = 'crime-utf8.csv'
+accidentFile='us_accidents_small.csv'
+
+
+
+def printAccident(info, lista):
+        accidentRead=lt.getElement(info['accidents'],0) 
+        print (accidentRead['Severity'])
+        print (lt.getElement(lst,0))
+        numAccidentes=controller.accidentSize(info)
+        for k in range (0, lt.size(lst)):
+            #print (lt.getElement(lst,k))                  #Aqui se imprimen los valores del mapa
+            dateCom=lt.getElement(lst,k)
+            
+            for i in range (0,numAccidentes):
+            #for i in range (0,10):    
+                accidentRead=lt.getElement(info['accidents'],i) 
+                #oneDate = datetime.datetime.strptime(accidentRead['Start_Time'], '%Y-%m-%d')
+                oneDate = accidentRead['Start_Time']
+                oneDate = datetime.datetime.strptime(oneDate, '%Y-%m-%d %H:%M:%S')
+                oneDate1 = datetime.datetime.strftime(oneDate,'%Y-%m-%d')
+                #print (oneDate1)
+                #oneDate = datetime.fromisoformat(oneDate)
+                #oneDate = datetime._parse_isoformat_date(oneDate)
+                #print ("  k: ", k, "   v:", v, "  ", i, ": " , accidentRead['ID']," ", accidentRead['Severity']," ",oneDate1)
+                #print (dateCom, "-->",oneDate1)
+                #input("")
+                if str(dateCom)==str(oneDate1):     
+                     print (dateCom, "-->", i, ": " , accidentRead['ID']," ", accidentRead['Severity'])
+            
+      
+        
+
 
 # ___________________________________________________
 #  Menu principal
@@ -47,14 +81,20 @@ crimefile = 'crime-utf8.csv'
 
 def printMenu():
     print("\n")
-    print("*******************************************")
-    print("Bienvenido")
-    print("1- Inicializar Analizador")
-    print("2- Cargar información de accidentes")
-    print("3- Requerimento 1")
-    print("4- Requerimento 2")
-    print("0- Salir")
-    print("*******************************************")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@") 
+    print("@@@@@@@@@                         RETO 3. Seguridad en las vias                @@@@@@@@@@@")   
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@") 
+    print("[1] Inicializar Analizador")
+    print("[2] Cargar información de accidentes")
+    print("[3] Requerimento 1: Conocer los accidentes en una fecha")
+    print("[4] Requerimento 2: Conocer los accidentes anteriores a una fecha")
+    print("[5] Requerimento 3: Conocer los accidentesen un rango de fechas")
+    print("[6] Requerimento 4: Conocer el estado con mas accidentes")
+    print("[7] Requerimento 5: Conocer los accidentes por rango de horas")
+    print("[8] Requerimento 6: Conocer las zona geografica mas accidentada")
+    print("[9] Requerimento 7: Usar el conjunto completo de datos")
+    print("[0]- Salir")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@") 
 
 
 """
@@ -62,22 +102,93 @@ Menu principal
 """
 while True:
     printMenu()
-    inputs = input('Seleccione una opción para continuar\n>')
+    inputs = input('Seleccione una opción para continuar >> ')
 
     if int(inputs[0]) == 1:
-        print("\nInicializando....")
-        # cont es el controlador que se usará de acá en adelante
+
+        print("\nInicializando lista Analyzer y mapa ordenado dateIndex....")
         cont = controller.init()
+        #lt.addLast(cont['accidents'],"Dato de prueba de de la lista.. OKKKK")
+        #dato=lt.getElement(cont['accidents'],0) 
+
+        print (cont['accidents'])
+        #print (dato)
+
+        input("Ahora la mapa ordenado dateIndex")
+        print (cont['dateIndex'])
+        print ("")
+        input("Se acaba de crear el Catalogo Analyzer con su respectiva lista y un mapa ordenado tipo BST")
+        # cont es el controlador que se usará de acá en adelante
+       
 
     elif int(inputs[0]) == 2:
-        print("\nCargando información de crimenes ....")
+
+        print("\n Cargando información de accidentes ....\n")
+
+        controller.loadData(cont, accidentFile)
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@") 
+        print (lt.getElement(cont['accidents'],0))
+        print (lt.getElement(cont['accidents'],controller.accidentSize(cont)))
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        #for i  in range (0,controller.accidentSize(cont)):
+        #   print (lt.getElement(cont['accidents'],i))
+        print ("")
+        print('Accidentes cargados: ' + str(controller.accidentSize(cont)))
+        print('Altura del arbol: ' + str(controller.indexHeight(cont)))
+        print('Elementos en el arbol: ' + str(controller.indexSize(cont)))
+        print('Menor Llave: ' + str(controller.minKey(cont)))
+        print('Mayor Llave: ' + str(controller.maxKey(cont)))
+        print("")
+
+        #accidentRead=lt.getElement(cont['accidents'],0) 
+        #print (accidentRead['Severity'])
+        input("Clic para continuar")
+       
 
     elif int(inputs[0]) == 3:
         print("\nBuscando crimenes en un rango de fechas: ")
+        
+        initialDate="2000-00-00"
+        #finalDate= "2000-00-00"
+        initialDate = input("Fecha Busqueda (YYYY-MM-DD): ")       
+        #finalDate = input("Rango Final (YYYY-MM-DD): ")
+        lst = controller.getAccidentsByRange(cont, initialDate, initialDate) 
+        
+        #print (lst)
+        print("\nTotal de llaves en el rango: " + str(lt.size(lst)))
+        print (initialDate)
+
+        #for k,v in lst.items():
+           #print (lt.getElement(lst,0))
+        #   print (v)
+        
+        printAccident(cont,lst)
+
+        """tamLista= lt.size(lst)   
+        for i in range (1, tamLista):
+               print (lt.getElement(lst,i))
+        
+      
+
+        """
+        input("Clic para continuar")
 
 
-    elif int(inputs[0]) == 4:
+
+    elif int(inputs[0]) == 5:
         print("\nRequerimiento No 1 del reto 3: ")
+
+        print("\nBuscando crimenes en un rango de fechas: ")
+        
+        initialDate="2000-00-00"
+        finalDate= "2000-00-00"
+        initialDate = input("Rango Inicial (YYYY-MM-DD): ")       
+        finalDate = input("Rango Final (YYYY-MM-DD): ")
+        lst = controller.getAccidentsByRange(cont, initialDate,finalDate) 
+        
+        #print (lst)
+        print("\nTotal de llaves en el rango: " + str(lt.size(lst)))
+        print (initialDate,finalDate)
 
     else:
         sys.exit(0)
